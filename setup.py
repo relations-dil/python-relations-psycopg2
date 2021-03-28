7e8974a457f5923546f4b6c912b45a7dfa77e55f
Gaffer Fitch <gaffer.fitch@gmail.com>
2021-03-28 07:24:07 -0400

    Limit after where (#15)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name="relations-psycopg2",
-    version="0.3.3",
+    version="0.3.4",
     package_dir = {'': 'lib'},
     py_modules = [
         'relations_psycopg2'
