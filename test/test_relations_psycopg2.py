24bdea57acdcbdaa122613dcd17f720ff2a22352
Gaffer Fitch <gaffer.fitch@gmail.com>
2021-05-31 09:34:08 -0400

    Proper keying

--- test/test_relations_psycopg2.py
+++ test/test_relations_psycopg2.py
@@ -32,7 +32,7 @@ class Meta(SourceModel):
     stuff = list
     things = dict
     pull = str, {"extract": "things__for__0___1"}
-    push = str, {"inject": "stuff__-1__4___1"}
+    push = str, {"inject": "stuff__-1__relations.io___1"}
 
 def subnet_attr(values, value):
 
@@ -564,7 +564,7 @@ class TestSource(unittest.TestCase):
             "name": "yep",
             "flag": True,
             "spend": 3.50,
-            "stuff": [1, [None, None, None, None, {"1": "sure"}]],
+            "stuff": [1, {"relations.io": {"1": "sure"}}],
             "things": {"for": [{"1": "yep"}]},
             "pull": "yep"
         })
